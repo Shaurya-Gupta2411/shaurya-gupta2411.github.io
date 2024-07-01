deb93f1df4f8110f130a8f292a155b5e0080e51a
Shaurya-Gupta2411 <shaurya@hotmail.co.uk>
2024-07-01 17:49:22 +0100

    new logo, more changes to content

---
+++ address.js
@@ -0,0 +1,81 @@
+let address = `
+<div
+												class="et_pb_column et_pb_column_1_4 et_pb_column_8  et_pb_css_mix_blend_mode_passthrough">
+
+
+												<div
+													class="et_pb_module et_pb_blurb et_pb_blurb_0  et_pb_text_align_left  et_pb_blurb_position_top et_pb_bg_layout_light">
+
+
+													<div class="et_pb_blurb_content">
+
+														<div class="et_pb_blurb_container">
+															<h4 class="et_pb_module_header"><span>UK</span></h4>
+															<div class="et_pb_blurb_description">112 Morden
+																Road<br />London<br /> SW19 3BP, United Kingdom</div>
+														</div>
+													</div> <!-- .et_pb_blurb_content -->
+												</div> <!-- .et_pb_blurb -->
+											</div> <!-- .et_pb_column -->
+											<div
+												class="et_pb_column et_pb_column_1_4 et_pb_column_8  et_pb_css_mix_blend_mode_passthrough">
+
+
+												<div
+													class="et_pb_module et_pb_blurb et_pb_blurb_0  et_pb_text_align_left  et_pb_blurb_position_top et_pb_bg_layout_light">
+
+
+													<div class="et_pb_blurb_content">
+
+														<div class="et_pb_blurb_container">
+															<h4 class="et_pb_module_header"><span>INDIA</span></h4>
+															<div class="et_pb_blurb_description">
+																<p>Plot 12A, Sector 21 D<br />Faridabad,
+																	Haryana,<br />121001, India</p>
+															</div>
+														</div>
+													</div> <!-- .et_pb_blurb_content -->
+												</div> <!-- .et_pb_blurb -->
+											</div> <!-- .et_pb_column -->
+											<div
+												class="et_pb_column et_pb_column_1_4 et_pb_column_9  et_pb_css_mix_blend_mode_passthrough">
+
+
+												<div
+													class="et_pb_module et_pb_blurb et_pb_blurb_1  et_pb_text_align_left  et_pb_blurb_position_top et_pb_bg_layout_light">
+
+
+													<div class="et_pb_blurb_content">
+
+														<div class="et_pb_blurb_container">
+															<h4 class="et_pb_module_header"><span>EU</span></h4>
+															<div class="et_pb_blurb_description">
+																<p>4/5 Handlowa Street<br />Rzeszow<br />35-103, Poland
+																</p>
+															</div>
+														</div>
+													</div> <!-- .et_pb_blurb_content -->
+												</div> <!-- .et_pb_blurb -->
+											</div> <!-- .et_pb_column -->
+											<div
+												class="et_pb_column et_pb_column_1_4 et_pb_column_10  et_pb_css_mix_blend_mode_passthrough et-last-child">
+
+
+												<div
+													class="et_pb_module et_pb_blurb et_pb_blurb_2  et_pb_text_align_left  et_pb_blurb_position_top et_pb_bg_layout_light">
+
+
+													<div class="et_pb_blurb_content">
+
+														<div class="et_pb_blurb_container">
+															<h4 class="et_pb_module_header"><span>Egypt</span></h4>
+															<div class="et_pb_blurb_description">
+																<p>4/5 Handlowa Street<br />Rzeszow<br />35-103, Poland
+																</p>
+															</div>
+														</div>
+													</div> <!-- .et_pb_blurb_content -->
+												</div> <!-- .et_pb_blurb -->
+											</div> <!-- .et_pb_column -->`;
+
+document.getElementById("address-content").innerHTML = address;                                            
---
+++ header.js
@@ -0,0 +1,76 @@
+let mainHeader = `
+<div class="container clearfix et_menu_container">
+				<div class="logo_container">
+					<span class="logo_helper"></span>
+					<a href="index.html">
+						<img src="wp-content/uploads/MRail/logo.png" alt="EWL International" id="logo"
+							data-height-percentage="85" />
+					</a>
+				</div>
+				<div id="et-top-navigation" data-height="80" data-fixed-height="40">
+					<nav id="top-menu-nav">
+						<ul id="top-menu" class="nav">
+							<li id="menu-item-533"
+								class="menu-item menu-item-type-post_type menu-item-object-page menu-item-533"><a
+									href="who-we-are.html">Who we are</a></li>
+							<li id="menu-item-1086"
+								class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children menu-item-1086">
+								<a href="#">Services</a>
+								<ul class="sub-menu">
+									<li id="menu-item-532"
+										class="menu-item menu-item-type-post_type menu-item-object-page menu-item-532">
+										<a href="project-programme-management.html">Project &#038; Programme
+											Management</a></li>
+									<li id="menu-item-531"
+										class="menu-item menu-item-type-post_type menu-item-object-page menu-item-531">
+										<a href="railways.html">Railways</a></li>
+									<li id="menu-item-530"
+										class="menu-item menu-item-type-post_type menu-item-object-page menu-item-530">
+										<a href="highways-roads.html">Highways &#038; Roads</a></li>
+									<li id="menu-item-529"
+										class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-459 current_page_item menu-item-529">
+										<a href="airports.html" aria-current="page">Airports</a></li>
+									<li id="menu-item-528"
+										class="menu-item menu-item-type-post_type menu-item-object-page menu-item-528">
+										<a href="transportation-advisory-services.html">Transportation Advisory
+											Services</a></li>
+									<li id="menu-item-527"
+										class="menu-item menu-item-type-post_type menu-item-object-page menu-item-527">
+										<a href="property-development.html">Property Development</a></li>
+									<li id="menu-item-526"
+										class="menu-item menu-item-type-post_type menu-item-object-page menu-item-526">
+										<a href="expert-deployment-services.html">Expert Deployment Services</a></li>
+								</ul>
+							</li>
+							<li id="menu-item-678"
+								class="menu-item menu-item-type-post_type menu-item-object-page menu-item-678"><a
+									href="projects.html">Projects</a></li>
+							<li id="menu-item-524"
+								class="menu-item menu-item-type-post_type menu-item-object-page menu-item-524"><a
+									href="contact-us.html">Contact us</a></li>
+						</ul>
+					</nav>
+
+
+
+
+					<div id="et_mobile_nav_menu">
+						<div class="mobile_nav closed">
+							<span class="select_page">Select Page</span>
+							<span class="mobile_menu_bar mobile_menu_bar_toggle"></span>
+						</div>
+					</div>
+				</div> <!-- #et-top-navigation -->
+			</div> <!-- .container -->
+			<div class="et_search_outer">
+				<div class="container et_search_form_container">
+					<form role="search" method="get" class="et-search-form" action="https://ewlinternational.co.uk/">
+						<input type="search" class="et-search-field" placeholder="Search &hellip;" value="" name="s"
+							title="Search for:" />
+					</form>
+					<span class="et_close_search_field"></span>
+				</div>
+			</div>
+            `;
+
+            document.getElementById("main-header").innerHTML = mainHeader;
